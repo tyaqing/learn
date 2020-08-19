edf49fb94d624a7ab8f5e5e96cc16f011bdbf878
Yaqing Teng <yaqingteng@YaqingdeMBP15.lan>
2020-08-19 13:11:58 +0800

    2 methods resolve this algrithm test

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -6,8 +6,31 @@
  */
 var containsDuplicate = function (nums) {
 
+    // 传统指针思路
+    // let l = 0, r = 1;
+    // while (l < nums.length) {
+    //     while (r < nums.length) {
+    //         if (nums[l] === nums[r]) {
+    //             return true;
+    //         } else {
+    //             r++;
+    //         }
+    //     }
+    //     l++;
+    //     r = l + 1;
+    // }
+    // return false;
+
+    // 使用Set
+    const set = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        set.add(nums[i]);
+    }
+    if (set.size === nums.length) return false;
+    else return true;
+
 };
 
 
-let result = containsDuplicate([1, 2, 3, 1]);
+let result = containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]);
 console.log(result);
