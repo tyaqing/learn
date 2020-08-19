62070780337c7cb6947f5fdece33990e94050dae
Yaqing Teng <yaqingteng@YaqingdeMBP15.lan>
2020-08-19 12:51:11 +0800

    add a new algorithm file

---
+++ contains-duplicate.js
@@ -0,0 +1,13 @@
+/**
+ * 217. 存在重复元素
+ * https://leetcode-cn.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+
+};
+
+
+let result = containsDuplicate([1, 2, 3, 1]);
+console.log(result);
