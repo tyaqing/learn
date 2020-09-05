997524bae91234186cbebb16e67aa22edfdae18e
tyaqing <626019610@qq.com>
2020-09-05 19:01:26 +0800

    添加antd pro相关文件

--- basic/src/promise.js
+++ basic/src/promise.js
@@ -1,9 +1,25 @@
-let p = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    //使用定时器来模拟异步
-    resolve(100);
-  }, 1000);
+class HD {
+  static PENDING = "pending";
+  static FULFILLED = "fulfilled";
+  static REJECTED = "rejected";
+  constructor(executor) {
+    this.staus = HD.PENDING;
+    this.value = null;
+    executor(this.resolve, this.reject);
+  }
+  resolve(value) {
+    console.log(this);
+    this.staus = HD.FULFILLED;
+    this.value = value;
+  }
+  reject(reason) {
+    this.staus = HD.REJECTED;
+    this.reason = reason;
+  }
+}
+
+let p = new HD((resolve, reject) => {
+  resolve(123);
 });
-p.then(function (data) {
-  console.log(data);
-});
+
+console.log(p);
