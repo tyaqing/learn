73e230a3269fbecb9ce696c296147771353207b0
tyaqing <626019610@qq.com>
2020-09-27 21:06:52 +0800

    新增react hooks

--- react-demo/src/page/Hooks.js
+++ react-demo/src/page/Hooks.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Hooks.css";
 export default function Hooks() {
   const [count, setCount] = useState(0);
   function inc() {
@@ -8,6 +9,17 @@ export default function Hooks() {
   useEffect(() => {});
   return (
     <div>
+      <div className="container">
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
       <div>{count}</div>
       <button onClick={inc}>
         <span>+</span>
