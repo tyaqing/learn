765024a23833b9e92cc995d88c36dc613e538940
tyaqing <626019610@qq.com>
2020-09-28 23:12:31 +0800

    更新一下今天的面试题

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer/counter";
@@ -11,10 +11,28 @@ const store = createStore(reducer);
 const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Hooks />
+      {/* <Hooks /> */}
+      <div id="div">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </Provider>,
     document.getElementById("root")
   );
 };
 render();
 store.subscribe(render);
+
+var value = 1;
+
+function foo() {
+  console.log(value);
+}
+
+function bar() {
+  var value = 2;
+  foo();
+}
+
+bar();
